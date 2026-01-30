a94ba0ba027e3f85d071fc7c34213eb92ddd5d93
Anna <anna.a.osipova@outlook.com>
2026-01-30 15:16:11 +0400

    Product Test is added

---
+++ src/test/java/tests/ProductsTest.java
@@ -0,0 +1,18 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class ProductsTest extends BaseTest {
+    @Test
+    public void checkGoodsAdded() {
+        loginPage.open();
+        loginPage.login("standard_user", "secret_sauce");
+        productsPage.isTitleIsDisplayed();
+        productsPage.addGoodsToCart("Sauce Labs Onesie");
+        productsPage.addGoodsToCart("Sauce Labs Fleece Jacket");
+        assertEquals(productsPage.checkCounterValue(), "2");
+        assertEquals(productsPage.checkCounterColor(), "rgba(226, 35, 26, 1)");
+    }
+}
