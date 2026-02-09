7ccee32cd762bac6b098df0d32311e37798f8e58
Anna <anna.a.osipova@outlook.com>
2026-02-09 23:54:59 +0400

    Page classes are updated

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -1,36 +1,48 @@
 package pages;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ProductsPage extends BasePage {
     private static final String ADD_TO_CART_PATTERN = "//*[text()='%s']" +
             "//ancestor::div[@class='inventory_item']//child::button[text()='Add to cart']";
-    private final By title = By.cssSelector("[data-test='title']");
+    private final By title = By.cssSelector(DATA_TEST_PATTERN.formatted("title"));
     private final By cartCounter = By.cssSelector(DATA_TEST_PATTERN.formatted("shopping-cart-badge"));
+    private final By cartLink = By.cssSelector(DATA_TEST_PATTERN.formatted("shopping-cart-link"));
+
 
     public ProductsPage(WebDriver driver) {
         super(driver);
     }
 
+    @Step("Проверяем отображение заголовка")
     public boolean isTitleIsDisplayed() {
         return driver.findElement(title).isDisplayed();
     }
 
-    public String getTitle() {
-        return driver.findElement(title).getText();
-    }
-
+    @Step("Добавляем товары в корзину")
     public void addGoodsToCart(String goodsName) {
         By addToCart = By.xpath(ADD_TO_CART_PATTERN.formatted(goodsName));
         driver.findElement(addToCart).click();
     }
 
+    public void addGoodsToCart(int goodsIndex) {
+        driver.findElements(By.xpath("//*[text()='Add to cart']")).get(goodsIndex).click();
+    }
+
+    @Step("Проверяем корректность каунтера")
     public String checkCounterValue() {
         return driver.findElement(cartCounter).getText();
     }
 
+    @Step("Проверяем цвет каунтера")
     public String checkCounterColor() {
         return driver.findElement(cartCounter).getCssValue("background-color");
+    }
+
+    @Step("Переходим на страницу корзины")
+    public void switchToCart() {
+        driver.findElement(cartLink).click();
     }
 }
